7f4a0fa13a423a1408416a6b6872c88690fdd76c
marcos <marcos.vinicius59533@gmail.com>
2024-06-16 19:45:45 -0300

    first commit

---
+++ src/main/java/com/marcos/primaryProject/PrimaryProjectApplication.java
@@ -0,0 +1,12 @@
+package com.marcos.primaryProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimaryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimaryProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/marcos/primaryProject/PrimaryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.primaryProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimaryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
